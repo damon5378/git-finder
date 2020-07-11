702e1ff12abbb89713a1fd65bf496954260a3ac4
damon5378 <iosifov.konstantin@gmail.com>
2020-07-11 20:52:17 +0300

    Created Users component & upd styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 
 class App extends React.Component {
   render() {
     return (
     <div className="App">
     <Navbar title="Github Finder" icon="fab fa-github"/>
-    <UserItem />
+    <div className="container">
+      <Users />
+    </div>
+    
     </div>
   );
   }
