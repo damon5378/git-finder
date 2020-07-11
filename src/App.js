17f868c20bd8d4b2f9f8860af01739de65bb434c
damon5378 <iosifov.konstantin@gmail.com>
2020-07-11 22:53:07 +0300

    Created Search component & upd styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
+import Search from './components/users/Search';
 
 class App extends React.Component {
 
@@ -15,10 +16,24 @@ class App extends React.Component {
     this.setState({
       loading: true
     })
-    const response = await axios.get('https://api.github.com/users');
+    const response = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
     this.setState({
       users: response.data,
+      loading: false
+    })
+  }
+
+  searchUsers = async (text) => {
+
+    this.setState({
+      loading: true
+    })
+
+    const response = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({
+      users: response.data.items,
       loading: false
     })
   }
@@ -28,6 +43,7 @@ class App extends React.Component {
     <div className="App">
     <Navbar title="Github Finder" icon="fab fa-github"/>
     <div className="container">
+      <Search searchUsers={this.searchUsers}/>
       <Users loading={this.state.loading} users={this.state.users}/>
     </div>
     
--- src/components/layout/Loader.jsx
+++ src/components/layout/Loader.jsx
@@ -4,7 +4,7 @@ import s from './Loader.module.css';
 const Loader = () => {
     return (
         <div>
-            <div class={s.lds_spinner}>
+            <div className={s.lds_spinner}>
                 <div></div><div></div><div></div><div></div><div></div><div></div><div></div>
                 <div></div><div></div><div></div><div></div><div></div></div>
         </div>
