5ae0798987ee677ad5ee6eee5a6e20ba2a2e2e75
damon5378 <iosifov.konstantin@gmail.com>
2020-07-11 20:26:14 +0300

    Created UserItem component & upd styles

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 
-export class Navbar extends Component {
+class Navbar extends Component {
     render() {
         return (
             <nav className="navbar bg-dark">
